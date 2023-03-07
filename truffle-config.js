b28e6fb007f62720fd32dbcc7cbc9a1ec38495cb
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-03-06 21:37:54 -0500

    working with geth --http

--- truffle-config.js
+++ truffle-config.js
@@ -67,7 +67,8 @@ module.exports = {
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+     network_id: "*",
+     from: '0x6Cd3321d937D130fa2d63153731D870fe2deAB84'       // Any network (default: none)
     },
     //
     // An additional network, but with some advanced options…
